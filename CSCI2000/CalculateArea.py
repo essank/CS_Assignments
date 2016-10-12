4b505fd4318fba9a3794d862e0ba56646e8733f0
Essa ✌️ <essank99@gmail.com>
2016-10-12 13:05:15 -0500

    Update CalculateArea.py

--- CSCI2000/CalculateArea.py
+++ CSCI2000/CalculateArea.py
@@ -1,19 +1,3 @@
-## Author: Issa Alkhamees
-## Due Date: October 5
-## Class: CSCI 2000
-## Instructor: Dr. Eaton
-## Program Assignment: 5
-## Program Title: CalculateArea
-## Program Description:
-##  This program calculates a company’s
-##  break-even point, the number of units of goods the
-##  company must manufacture and sell in order to break even.
-##  It outputs the break-even point.
-##
-## Plagiarism Statement:
-##  I swear that I have not received or given aid on this program
-##  beyond guidelines of the course including giving or receiving
-##  specific code content.
 
 
 #Print the options
