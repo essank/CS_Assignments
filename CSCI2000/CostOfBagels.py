861931443098b65de80e69c65b45acda109c9ac7
Essa ✌️ <essank99@gmail.com>
2016-10-12 13:04:42 -0500

    Update CostOfBagels.py

--- CSCI2000/CostOfBagels.py
+++ CSCI2000/CostOfBagels.py
@@ -1,19 +1,4 @@
-## Author: Issa Alkhamees
-## Due Date: September 28
-## Class: CSCI 2000
-## Instructor: Dr. Eaton
-## Program Assignment: 3
-## Program Title: Cost of Bagels
-## Program Description:
-##  This program calculates bagels discount
-##  it will charges 75 cents per bagel for orders of
-##  lees than a half dozen and 60 cents per bagel
-##  for half dozen and more.
-##
-## Plagiarism Statement:
-##  I swear that I have not received or given aid on this program
-##  beyond guidelines of the course including giving or receiving
-##  specific code content.
+
 
 # asking how many bagels will be there
 numberOfBagels = int(input("Enter number of bagels: "))
