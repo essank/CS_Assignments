d06baec62234965dea3f5c2b5196abfb3f6b9c0e
Essa <essank99@gmail.com>
2017-01-25 20:21:17 -0600

    add commit

--- CSCI2003/Assignment1/src/Employee.java
+++ CSCI2003/Assignment1/src/Employee.java
@@ -1,2 +1,16 @@
+// Name: Issa Alkhamees
+//
+// Date Assigned: 1/24
+//
+// Course: CSCI 2003 42733
+//
+// Date Due: 1/26
+//
+// Instructor: Ms. Greer
+//
+// File name: Employee
+//
+// Program Description: simulate a company roster
+
 public class Employee {
 }
--- CSCI2003/Assignment1/src/EmployeeTester.java
+++ CSCI2003/Assignment1/src/EmployeeTester.java
@@ -1,2 +1,16 @@
+// Name: Issa Alkhamees
+//
+// Date Assigned: 1/24
+//
+// Course: CSCI 2003 42733
+//
+// Date Due: 1/26
+//
+// Instructor: Ms. Greer
+//
+// File name: EmployeeTester
+//
+// Program Description: simulate a company roster
+
 public class EmployeeTester {
 }
