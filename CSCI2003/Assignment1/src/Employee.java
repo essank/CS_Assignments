a511d9ba5626983f8e1c1d6c0b411a6530a24262
Essa <essank99@gmail.com>
2017-01-25 20:43:54 -0600

    add code to Employee

--- CSCI2003/Assignment1/src/Employee.java
+++ CSCI2003/Assignment1/src/Employee.java
@@ -12,5 +12,71 @@
 //
 // Program Description: simulate a company roster
 
-public class Employee {
+public class Employee
+{
+
+    private String EmployeeName;
+    private int EmployeeId;
+    private  String EmployeeDepartment;
+    private  String EmployeePosition;
+
+    // no-argument constractor
+    public Employee ()
+    {
+        EmployeeName = "";
+        EmployeeId = 0;
+        EmployeeDepartment = "";
+        EmployeePosition = "";
+    }
+
+    //argument constractor
+    public Employee (String EN, int EI, String ED, String EP)
+    {
+        EmployeeName = EN;
+        EmployeeId = EI;
+        EmployeeDepartment = ED;
+        EmployeePosition = EP;
+    }
+
+    //setters
+    public void setEmployeeName(String EN)
+    {
+        EmployeeName = EN;
+    }
+
+    public void setEmployeeId (int EI)
+    {
+        EmployeeId = EI;
+    }
+
+    public void setEmployeeDepartment(String ED)
+    {
+        EmployeeDepartment = ED;
+    }
+
+    public void setEmployeePosition(String EP)
+    {
+        EmployeePosition = EP;
+    }
+
+    //getters
+    public String getEmployeeName()
+    {
+        return EmployeeName;
+    }
+
+    public int getEmployeeId()
+    {
+        return EmployeeId;
+    }
+
+    public String getEmployeeDepartment()
+    {
+        return EmployeeDepartment;
+    }
+
+    public String getEmployeePosition()
+    {
+        return EmployeePosition;
+    }
 }
