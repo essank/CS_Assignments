6018247300973cd129dcba9ed71167cf769d1c66
Essa <essank99@gmail.com>
2017-02-06 13:06:28 -0600

    Update Room.java

--- CSCI2003/Assignment 2/src/Room.java
+++ CSCI2003/Assignment 2/src/Room.java
@@ -1,12 +1,3 @@
-// Name: Issa Alkhamees Date Assigned: 1/31
-//
-// Course: CSCI 2003 42733 Date Due: 2/10
-// Instructor: Ms. Greer
-//
-// File name: Room.java
-//
-// Program Description: simulate an online store that sells paint.
-
 public class Room
 {
     private int numberOfWalls;
