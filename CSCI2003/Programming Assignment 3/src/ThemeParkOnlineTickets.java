98236148732add7b391bcaa4a42ff47f5ff8dcfe
Essa <essank99@gmail.com>
2017-02-19 22:11:27 -0600

    add more codes :\

--- CSCI2003/Programming Assignment 3/src/ThemeParkOnlineTickets.java
+++ CSCI2003/Programming Assignment 3/src/ThemeParkOnlineTickets.java
@@ -20,7 +20,7 @@ public class ThemeParkOnlineTickets {
 
         System.out.println("         Welcome! Please complete your ticket order below!");
         System.out.println("=================================================================");
-        System.out.println("    +-----------------------+-----------------+-----------------+");
+        System.out.println("\n    +-----------------------+-----------------+-----------------+");
         System.out.println("    |      THEME PARK       |   ADULT TICKET  |   CHILD TICKET  |");
         System.out.println("+---+-----------------------+-----------------+-----------------+");
         System.out.println("| 1 |    Universal Studios  |      $105.00    |      $100.00    |");
@@ -34,7 +34,52 @@ public class ThemeParkOnlineTickets {
         System.out.print("\nChoose a Theme Park (1 - 3): ");
         int choose = key.nextInt();
 
+        if (choose == 1)
+        {
+            park1.setName("Universal Studios");
+            System.out.println("\n\n+-----------------------+-----------------+-----------------+");
+            System.out.println("|      THEME PARK       |   ADULT TICKET  |   CHILD TICKET  |");
+            System.out.println("+-----------------------+-----------------+-----------------+");
+            System.out.println("|    Universal Studios  |      $105.00    |      $100.00    |");
+            System.out.println("+-----------------------+-----------------+-----------------+");
+            park1.setNumberAdultTicket(105);
+            park1.setNumberChildTicket(100);
+        }
 
+        if (choose == 2)
+        {
+            park1.setName("Magic Kingdom");
+            System.out.println("\n\n+-----------------------+-----------------+-----------------+");
+            System.out.println("|      THEME PARK       |   ADULT TICKET  |   CHILD TICKET  |");
+            System.out.println("+-----------------------+-----------------+-----------------+");
+            System.out.println("|     Magic Kingdom     |      $124.00    |      $118.00    |");
+            System.out.println("+-----------------------+-----------------+-----------------+");
+            park1.setNumberAdultTicket(124);
+            park1.setNumberChildTicket(118);
+        }
+
+        if (choose == 3)
+        {
+            park1.setName("Epcot");
+            System.out.println("\n\n+-----------------------+-----------------+-----------------+");
+            System.out.println("|      THEME PARK       |   ADULT TICKET  |   CHILD TICKET  |");
+            System.out.println("+-----------------------+-----------------+-----------------+");
+            System.out.println("|         Epcot         |      $114.00    |      $108.00    |");
+            System.out.println("+-----------------------+-----------------+-----------------+");
+            park1.setNumberAdultTicket(114);
+            park1.setNumberChildTicket(108);
+        }
+
+        System.out.print("\nNumber of Adult Tickets: ");
+        int adultTicket = key.nextInt();
+
+        System.out.print("Number of Child Tickets: ");
+        int childTicket = key.nextInt();
+
+        park1.setPriceAdultTicket(adultTicket);
+        park1.setPriceChildTicket(childTicket);
+
+        park1.order();
     }
 }
 
