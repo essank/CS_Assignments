453d2e15a14c7d63a4634a1dd63c81a3ae403e86
Essa <essank99@gmail.com>
2017-03-22 17:52:16 -0500

    creating DiceGame file

---
+++ CSCI2003/Programming Assignment 5/src/DiceGame.java
@@ -0,0 +1,12 @@
+// Name: Issa Alkhamees         Date Assigned: 3/16/2017
+//
+// Course: CSCI 2003 60357      Date Due: 3/23/2017
+//
+// Instructor: Ms. Greer
+//
+// File name: DiceGame
+//
+// Program Description: dice game called “Run for it!”.
+
+public class DiceGame {
+}
