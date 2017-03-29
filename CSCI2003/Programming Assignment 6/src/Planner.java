ca72bd09b2bf6d6f1b7f88d4d93a60bf39c6c779
Essa <essank99@gmail.com>
2017-03-28 19:04:29 -0500

    The program allows the user to

---
+++ CSCI2003/Programming Assignment 6/src/Planner.java
@@ -0,0 +1,15 @@
+// Name: Issa Alkhamees          Date Assigned: 3/23
+//
+// Course: CSCI 2003 60357       Date Due: 3/28
+//
+// Instructor: Ms. Greer
+//
+// File name: Planner.java
+//
+// Program Description: The program allows the user to
+// add a new event to the planner, display all the events
+// currently stored in the planner, or to quit the program.
+
+
+public class Planner {
+}
