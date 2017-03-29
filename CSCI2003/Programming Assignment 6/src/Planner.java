c7f46328b9e1d4e7e54eb5473b8de8bc9bab23c1
Essa <essank99@gmail.com>
2017-03-29 17:16:38 -0500

    adding code 6

--- CSCI2003/Programming Assignment 6/src/Planner.java
+++ CSCI2003/Programming Assignment 6/src/Planner.java
@@ -10,6 +10,124 @@
 // add a new event to the planner, display all the events
 // currently stored in the planner, or to quit the program.
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.PrintWriter;
 
-public class Planner {
+public class Planner
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+
+        Scanner in = new Scanner(System.in); // scanner
+
+        System.out.println("Personal Planner");
+        System.out.println("================");
+        boolean done = true;
+
+        //creating variables
+        String date = " ";
+        String time = " ";
+        String location = " ";
+        String event = " ";
+
+        while (done)
+        {
+            //displaying the options
+            System.out.println("\nOptions:");
+            System.out.println("========");
+            System.out.println("1. Add Event");
+            System.out.println("2. Display Events");
+            System.out.println("3. Quit");
+
+            //take the input from user
+            System.out.print("\nEnter options (1, 2, or 3): ");
+            int userOption = in.nextInt();
+
+            System.out.print("\n");
+
+            if (userOption == 1)
+            {
+                File infile = new File("planner.txt"); // file to read from
+                PrintWriter outfile = new PrintWriter("records.txt"); // file to write in
+                Scanner imn = new Scanner(infile);
+
+                while (imn.hasNextLine())
+                {
+                    outfile.println(imn.nextLine());
+                }
+
+                //close open files
+                imn.close();
+                outfile.close();
+
+                //object to write to the file planner
+                PrintWriter outfile1 = new PrintWriter("planner.txt");
+
+                System.out.print("Enter date (MM/DD/YY): ");
+                in.nextLine();
+                date = in.nextLine();
+
+                System.out.print("Enter time (HH:MM): ");
+                time = in.nextLine();                     //asking user for input
+
+                System.out.print("Enter location: ");
+                location = in.nextLine();
+
+                System.out.print("Enter event: ");
+                event = in.nextLine();
+
+                File k = new File("records.txt");  //file object
+                Scanner ifile1 = new Scanner(k);            //object to read from file
+
+                //writing from the temporary file to planner file
+                while (ifile1.hasNextLine())
+                {
+                    outfile.println(ifile1.nextLine());
+                }
+
+
+                outfile.println(date);
+                outfile.println(time);
+                outfile.println(location);
+                outfile.println(event);
+                outfile.println(" ");
+
+                //close open files
+                outfile.close();
+                ifile1.close();
+            }
+
+            else if (userOption == 2)
+            {
+
+                File outfile2 = new File("planner.txt");   //file object
+                Scanner infile = new Scanner(outfile2);  //object to read from file
+
+                if (infile.hasNextLine())
+                {
+
+                    while (infile.hasNextLine())
+                    {
+                        System.out.println(infile.nextLine());
+                    }
+                }
+
+                else
+                {
+                    System.out.println("No events added!");
+                }
+
+                //close open files
+                infile.close();
+            }
+
+            else if (userOption == 3)
+            {
+                //end boolean
+                done = false;
+            }
+        }
+    }
 }
