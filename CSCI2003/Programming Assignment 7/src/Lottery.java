02bbb2380e804a1c9c0eb2713438b99ad0670a43
Essa <essank99@gmail.com>
2017-04-10 19:00:01 -0500

    adding file 7

---
+++ CSCI2003/Programming Assignment 7/src/Lottery.java
@@ -0,0 +1,15 @@
+// Name: Issa Alkhamees          Date Assigned: 3/6
+//
+// Course: CSCI 2003 60357       Date Due: 4/13
+//
+// Instructor: Ms. Greer
+//
+// File name: Lottery.java
+//
+// Program Description: allowed to play as many times as they want,
+// and once they quit the game, it shows their total winnings for the game.
+// The user has 3 options when the game begins: Choose Numbers, Check Winnings, and Quit.
+
+
+public class Lottery {
+}
