6bd2c509fb8cefaea6e35f4b5b80198c9084d547
Essa <essank99@gmail.com>
2017-04-10 19:23:15 -0500

    add code

--- CSCI2003/Programming Assignment 7/src/Lottery.java
+++ CSCI2003/Programming Assignment 7/src/Lottery.java
@@ -10,6 +10,35 @@
 // and once they quit the game, it shows their total winnings for the game.
 // The user has 3 options when the game begins: Choose Numbers, Check Winnings, and Quit.
 
+import java.util.Random;    //importing Random class
+import java.util.Scanner;   //importing Scanner class
 
 public class Lottery {
+    public static void main (String[] args)
+    {
+        System.out.print("Instant Lottery");
+        System.out.print("\n===============");
+        System.out.print("\n\nOptions:" +
+                "\n========" +
+                "\n1. Choose Numbers" +
+                "\n2. Check Winnings" +
+                "\n3. Quit");
+
+        Random generator = new Random(); // create random object
+        Scanner in = new Scanner(System.in); // create scanner object
+
+        System.out.print("\n\nEnter choice (1, 2, or 3):");
+        int choice = in.nextInt();
+
+        System.out.print("\n---------------------------------");
+
+        System.out.print("\nNumber Options:" +
+                "\n===============" +
+                "\n1. Choose Your Numbers" +
+                "\n2. Quick Pick");
+
+        System.out.print("\n\nEnter choice (1 or 2):");
+        int options = in.nextInt();
+
+    }
 }
