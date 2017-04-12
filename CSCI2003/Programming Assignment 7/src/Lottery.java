c89e6cff3e7870b3f54858c09a98eec960f3b429
Essa <essank99@gmail.com>
2017-04-12 00:52:23 -0500

    adding more code

--- CSCI2003/Programming Assignment 7/src/Lottery.java
+++ CSCI2003/Programming Assignment 7/src/Lottery.java
@@ -16,29 +16,91 @@ import java.util.Scanner;   //importing Scanner class
 public class Lottery {
     public static void main (String[] args)
     {
+
+        int qui = 3;
+        int sum = 0;
+        boolean num = false;
+
         System.out.print("Instant Lottery");
         System.out.print("\n===============");
-        System.out.print("\n\nOptions:" +
-                "\n========" +
-                "\n1. Choose Numbers" +
-                "\n2. Check Winnings" +
-                "\n3. Quit");
 
         Random generator = new Random(); // create random object
         Scanner in = new Scanner(System.in); // create scanner object
 
-        System.out.print("\n\nEnter choice (1, 2, or 3):");
-        int choice = in.nextInt();
+        int [] Player_Numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        int [] Winning_Numbers = {};
 
-        System.out.print("\n---------------------------------");
 
-        System.out.print("\nNumber Options:" +
-                "\n===============" +
-                "\n1. Choose Your Numbers" +
-                "\n2. Quick Pick");
+        while(!num)
+        {
+            System.out.println("\n\nOptions: " +
+                    "\n========" +
+                    "\n1. Choose Numbers" +
+                    "\n2. Check Winnings" +
+                    "\n3. Quit");
 
-        System.out.print("\n\nEnter choice (1 or 2):");
-        int options = in.nextInt();
+            System.out.print("\nEnter choice (1, 2, or 3): ");
+            int options = in.nextInt();
 
+            System.out.println("\n---------------------------------");
+
+            while (options != 3)
+            {
+
+                try
+                {
+                    if (options == 1)
+                    {
+                        System.out.print("\nNumber Options:\n" +
+                                "===============\n" +
+                                "1. Choose Your Numbers\n" +
+                                "2. Quick Pick");
+                        System.out.print("\nEnter choice (1 or 2): ");
+                        int choice = in.nextInt();
+
+                        if (choice == 1)
+                        {
+                            System.out.println("\n\nChoose 5 Different Numbers");
+                            System.out.println("\n1 2 3 4 5 6 7 8 9 10\n" +
+                                    "11 12 13 14 15 16 17 18 19 20");
+
+                            System.out.print("Number 1: ");
+                            int Number1 = in.nextInt();
+
+                            System.out.print("Number 2: ");
+                            int Number2 = in.nextInt();
+
+                            System.out.print("Number 3: ");
+                            int Number3 = in.nextInt();
+
+                            System.out.print("Number 4: ");
+                            int Number4 = in.nextInt();
+
+                            System.out.print("Number 5: ");
+                            int Number5 = in.nextInt();
+
+                            System.out.printf("\nYour Numbers: %d %d %d %d %d", Number1, Number2,  Number3, Number4, Number5);
+                        }
+
+                        else if(choice == 2)
+                        {
+                            System.out.println("\nYour Numbers: ");
+                        }
+                    }
+
+                }
+
+                catch (NumberFormatException except)
+                {
+
+                }
+
+                finally
+                {
+
+                }
+            }
+        }//while
     }
 }
