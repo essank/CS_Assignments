a7d9149bdef9e2a9f2a261712d58c909125403a8
Essa <essank99@gmail.com>
2017-04-30 15:18:15 -0500

    add code to Course file

--- CSCI2003/Programming Assignments 9/src/Course.java
+++ CSCI2003/Programming Assignments 9/src/Course.java
@@ -7,10 +7,71 @@
 //
 // Program Description: allows teachers to access and/or update course information for the Computer Science Department.
 
-public class Course {
-    public String CourseName = null;
-    public int CourseCode = 0;
-    public int SectionNumber = 0;
-    public int AvailableSeats = 0;
+public class Course
+{
+    private String CourseName;
+    private int CourseCode;
+    private int SectionNumber;
+    private int AvailableSeats;
+
+    //no-argument constructor
+    public Course()
+    {
+        CourseName = " ";
+        CourseCode = 0;
+        SectionNumber = 0;
+        AvailableSeats = 0;
+    }
+
+    //argument constructor
+    public Course (String CName, int CCode, int SNum, int ASeats)
+    {
+        CourseName = CName;
+        CourseCode = CCode;
+        SectionNumber = SNum;
+        AvailableSeats = ASeats;
+    }
+
+    //seaters
+    public void setCourseName(String CName)
+    {
+        CourseName = CName;
+    }
+
+    public void setCourseCode(int CCode)
+    {
+        CourseCode = CCode;
+    }
+
+    public void setSectionNumber(int SNum)
+    {
+        SectionNumber = SNum;
+    }
+
+    public void setAvailableSeats(int ASeats)
+    {
+        AvailableSeats = ASeats;
+    }
+
+    //getters
+    public String getCourseName()
+    {
+        return CourseName;
+    }
+
+    public int getCourseCode()
+    {
+        return CourseCode;
+    }
+
+    public int getSectionNumber()
+    {
+        return SectionNumber;
+    }
+
+    public int getAvailableSeats()
+    {
+        return AvailableSeats;
+    }
 
 }
