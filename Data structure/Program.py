a6c60376ef0615875d032418ce3c1e8a98aab25e
Essankov <essank99@gmail.com>
2018-01-18 19:57:35 -0600

    adding print

---
+++ Data structure/Program.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+'''
+name: Issa Alkhamees
+Class and Section: CS 222 02
+Assignment: Program Assignment01
+Due Date: 1/26
+Date Turn in:
+    Program Description: this program for ML and NL
+'''
+
+def menu():
+    print('''
+    1.    List all winning teams from the AL
+    2.    List all winning teams from the NL
+    3.    For a specific team, how many times did they win
+    4.    For a specific team, how many times were they in the World Series 
+    5.    For a specific year, who won and who lost
+    6.    Quit
+    ''')
