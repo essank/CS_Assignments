73e2c1ae92db25152ebfe40f244893c89283d5be
Essankov <essank99@gmail.com>
2018-01-18 20:01:57 -0600

    adding commints

--- Data structure/Program.py
+++ Data structure/Program.py
@@ -5,11 +5,14 @@ name: Issa Alkhamees
 Class and Section: CS 222 02
 Assignment: Program Assignment01
 Due Date: 1/26
-Date Turn in:
-    Program Description: this program for ML and NL
+Date Turn in: 1/26
+Program Description: this program for ML and NL
 '''
 
+# create a function
 def menu():
+
+    #  print options 
     print('''
     1.    List all winning teams from the AL
     2.    List all winning teams from the NL
@@ -18,3 +21,7 @@ def menu():
     5.    For a specific year, who won and who lost
     6.    Quit
     ''')
+
+
+
+
