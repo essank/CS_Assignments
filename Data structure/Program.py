735a8413f8c88c64d7082c0723f7213d6fbf96a2
Essankov <essank99@gmail.com>
2018-01-21 02:29:17 -0600

    adding commints

--- Data structure/Program.py
+++ Data structure/Program.py
@@ -14,14 +14,18 @@ def menu():
 
     #  print options 
     print('''
-    1.    List all winning teams from the AL
-    2.    List all winning teams from the NL
-    3.    For a specific team, how many times did they win
-    4.    For a specific team, how many times were they in the World Series 
-    5.    For a specific year, who won and who lost
-    6.    Quit
+    1.List all winning teams from the AL
+    2.List all winning teams from the NL
+    3.For a specific team, how many times did they win
+    4.For a specific team, how many times were they in the World Series 
+    5.For a specific year, who won and who lost
+    6.Quit
     ''')
 
+    # read the file
+    inFile = open("worldSeries.txt", "r")
 
 
 
+    # close the file
+    inFile.close()
