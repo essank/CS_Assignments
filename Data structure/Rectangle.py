d558cef89c03a13f52637a537ec159959713a787
Essankov <essank99@gmail.com>
2018-02-05 02:33:18 -0600

    create Rectangle class

---
+++ Data structure/Rectangle.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python
+
+class Rectangle:
+    def __init__(self, width = 1, height = 2):
+        self.width = width
+        self.height = height
+
+    def getPerimeter(self):
+        return 2 * self.height + 2 * self.width
+
+    def getArea(self):
+        return self.height * self.width
---
+++ Data structure/test.py
@@ -0,0 +1,6 @@
+#!usr/bin/env python
+
+from Rectangle import Rectangle
+
+c = Rectangle(5,10)
+print(c.getPerimeter())
