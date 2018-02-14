b71429cfe583984ab6b8611de5853ace64f6d7fd
Essankov <essank99@gmail.com>
2018-02-13 19:11:58 -0600

    create employee

--- Data structure/Employee.py
+++ Data structure/Employee.py
@@ -1,16 +1,18 @@
+
+#Employee clas
 class Employee:
     def __init__(self, employeeName, employeeNumber):
         self.employeeName = employeeName
         self.employeeNumber = employeeNumber
-
+    #set Employee Name
     def setEmployeeName(self, employeeName):
         self.employeeName = employeeName
-
+    #set Employee Number
     def setEmployeeNumber(self, employeeNumber):
         self.employeeNumber = employeeNumber
-
+    #get Employee Name
     def getEmployeeName(self):
         return self.employeeName
-
+    #get Employee Number
     def getEmployeeNumber(self):
         return self.employeeNumber
