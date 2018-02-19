73f535a4ed9e879ab552845174f0d23e67e090fc
Essankov <essank99@gmail.com>
2018-02-18 20:05:27 -0600

    adding more code

--- Data structure/Employee.py
+++ Data structure/Employee.py
@@ -17,6 +17,35 @@ class Employee:
     def getEmployeeNumber(self):
         return self.employeeNumber
 
+
+    def __str__(self):
+        msg = "Employee name: " + self.getEmployeeName()
+        msg += "\nEmployee number" + str(self.getEmployeeNumber())
+        return msg
+
 class ProductionWorker(Employee):
-    def __init__(self, shiftNumber, payRate ):
-        Employee.__init__(self, shiftNumber, payRate)
+    def __init__(self, shiftNumber, payRate, employeeName, employeeNumber):
+        Employee.__init__(self, shiftNumber, payRate, employeeName, employeeNumber)
+        self.hourlyPayRate = payRate
+        self.shiftNumber = shiftNumber
+
+    def setHourlyPayRate(self, payRate):
+        self.hourlyPayRate = payRate
+
+    def getHourlyPayRate(self):
+        return self.hourlyPayRate 
+
+    def setShiftNumber(self, shiftNumber):
+        self.shiftNumber = shiftNumber
+
+    def getShiftNumber(self):
+        return self.shiftNumber
+
+    def __str__(self):
+        msg = Employee.__str__(self)
+        msg += "\n Hourly Pay Rate: " + str(self.getHourlyPayRate())
+        msg += "\n Shift Number: " + str(self.getshiftNumber())
+        return msg
+
+
+
